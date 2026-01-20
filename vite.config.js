265a31514e5b3b7c4b7baa0dbf89e7e5cfe387a1
Michael-Bui <220042409+Keepgoing-30@users.noreply.github.com>
2026-01-20 19:16:44 +0000

    Fixed profile image

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
+  // Đường dẫn cơ sở để deploy lên GitHub Pages
+  base: '/Portfolio-Website-Michael-Bui/', 
   plugins: [react()],
 })
