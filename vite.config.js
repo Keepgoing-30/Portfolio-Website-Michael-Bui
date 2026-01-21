8ec1457d232b8e29346a4cebe30d4ad3642a3c1c
Daniel Teleginski Camargo <daniel.camargo@ensign.edu>
2026-01-20 18:26:43 -0700

    chore: adding code quality tools (biome)

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  // Đường dẫn cơ sở để deploy lên GitHub Pages
-  base: '/Portfolio-Website-Michael-Bui/', 
-  plugins: [react()],
-})
+	// Đường dẫn cơ sở để deploy lên GitHub Pages
+	base: '/Portfolio-Website-Michael-Bui/',
+	plugins: [react()],
+});
